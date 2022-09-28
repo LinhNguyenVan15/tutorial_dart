48c95a0ae95e9bd454f68c121e7a6688f675132a
VanLinh-Flutter Dev <57564857+LinhNguyenVan15@users.noreply.github.com>
2022-09-28 13:50:23 +0700

    var in dart

--- dart_tutorial.dart
+++ dart_tutorial.dart
@@ -1,4 +1,8 @@
-main() {
+main(List<String> arguments) {
   //in ra dòng chữ
-  print("Hello World!");
+  var firstName = "Nguyễn";
+  var lastName = "Văn Linh";
+  print('firstname:$firstName, lastname:$lastName');
+  String fullName = firstName + ' ' + lastName;
+  print('fullname:$fullName');
 }
