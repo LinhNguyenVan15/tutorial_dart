da867c54562eed76eeeef57e6a4a14f0701254e4
VanLinh-Flutter Dev <linh39509667@gmail.com>
2022-10-11 00:48:50 +0700

    list set map tutorial

---
+++ list_set_map_tutorial.dart
@@ -0,0 +1,15 @@
+
+  main(List<String> arguments){
+    
+    //list
+  List <int> list=[1,2,3,4,5,6,7];
+  list.add(7);
+  print(list);
+
+  // create list object 
+  List<Object> list1=[1,2,3,4,5,6.3,"linhnguyen"];
+  list1.add("hello");
+  print(list1);
+
+
+}
